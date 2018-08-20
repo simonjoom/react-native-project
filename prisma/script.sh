e19b9aeb7a2b54bb3adb68407f902138792b811c
simon <simon@skiscool.com>
2018-08-20 09:11:45 +0200

    to install prisma with apollo-server

--- prisma/script.sh
+++ prisma/script.sh
@@ -4,6 +4,4 @@ cd ./packagespug
 find . -type f -name "*" ! -name ".DS_Store" ! -name "*.txt" > file.txt
 tar -c -T file.txt | tar -C ../node_modules -x 
 rm -Rf ./node_modules/graphql/
-npm link apollo-server-express
-npm link apollo-server-core
 cp ./scripts/ApolloServer.js ./node_modules//apollo-server-express/dist/
