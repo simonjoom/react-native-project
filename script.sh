14331171eb78cd5a99bdb1023b9594f263b2be5e
simon <simon@skiscool.com>
2018-08-23 16:40:58 +0200

    install

--- script.sh
+++ script.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 # deep copy script
 yarn add react-dev-utils@6.0.0-next.3e165448
+cp -Rf ./react-native-vector-icons ./node_modules/
+cp -Rf ./react-native-web-modal ./node_modules/
+cp -Rf ./react-scipts ./node_modules/
 rm -Rf ./node_modules/expo-web/node_modules/
 cp -Rf ./babel-eslint ./node_modules/
