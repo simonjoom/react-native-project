d7cbf2dff135b2d3c30c98abc75adc5a2becf82c
simon <simon@skiscool.com>
2018-08-25 20:47:08 +0200

    eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,35 +1,39 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "es6": true
-    },
-    "extends": "eslint:recommended",
-    "parserOptions": {
-        "ecmaFeatures": {
-            "jsx": true
-        },
-        "ecmaVersion": 2018,
-        "sourceType": "module"
-    },
-    "plugins": [
-        "react"
-    ],
-    "rules": {
-        "indent": [
-            "error",
-            "space"
-        ],
-        "linebreak-style": [
-            "error",
-            "unix"
-        ],
-        "quotes": [
-            "error",
-            "double"
-        ],
-        "semi": [
-            "error",
-            "always"
-        ]
+  env: {
+    browser: true,
+    es6: true,
+    "react-native/react-native": true
+  },
+  extends: [
+    "standard",
+    "plugin:flowtype/recommended",
+    "plugin:react/recommended",
+    "prettier",
+    "prettier/flowtype",
+    "prettier/react",
+    "prettier/standard", 
+    "plugin:react-native/all"
+  ],
+  plugins: ["flowtype", "react","react-native", "prettier", "standard"],
+  parserOptions: {
+    sourceType: "module",
+    ecmaFeatures: {
+      jsx: true
     }
+  },
+  rules: {    
+  "jsx-a11y/href-no-hash": "off",
+    "jsx-a11y/anchor-is-valid": ["warn", { "aspects": ["invalidHref"] }]
+    "prettier/prettier": "error"
+  },
+
+  settings: {
+    react: {
+      createClass: "createReactClass", // Regex for Component Factory to use,
+      // default to "createReactClass"
+      pragma: "React", // Pragma to use, default to "React"
+      version: "16.0", // React version, default to the latest React stable release
+      flowVersion: "0.53" // Flow version
+    }
+  }
 };
