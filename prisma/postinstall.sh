86a20f81a35c56b61a2d84d1143e4c76cab31285
simon <simon@skiscool.com>
2018-08-28 03:11:39 +0200

    Fix bug dependencies subscription

--- prisma/postinstall.sh
+++ prisma/postinstall.sh
@@ -5,6 +5,8 @@ find . -type f -name "*" ! -name ".DS_Store" ! -name "*.txt" > file.txt
 tar -c -T file.txt | tar -C ../node_modules -x 
 cd ..
 rm -Rf ./node_modules/graphql/
+cp -Rf ./scripts/graphql-binding ./node_modules/
+cp -Rf ./scripts/prisma-binding ./node_modules/
 cp ./scripts/ApolloServer.js ./node_modules/apollo-server-express/dist/
 echo "Succeed! The Server is ready to be run now"
 echo "On root type: npm run server"
